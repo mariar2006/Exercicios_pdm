4a5ab47a8f5953a1d1522fcf440f160215397697
mariar2006 <maria.r2006@aluno.ifsc.edu.br>
2024-09-09 11:53:44 -0300

    Sorteio

---
+++ app/src/main/java/com/example/contador/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.contador;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+    TextView text;
+    int cont;
+
+    TextView txtNum;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button= findViewById(R.id.bntclick);
+        txtNum = findViewById(R.id.txtNum);
+        button.setText("click");
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Random random = new Random();
+                Button b=(Button) view;
+                int ninicial=50;
+                int nfinal=100;
+                int nsorteado=random.nextInt(10);
+
+                txtNum.setText(Integer.toString(nsorteado));
+            }
+        });
+    }
+}
